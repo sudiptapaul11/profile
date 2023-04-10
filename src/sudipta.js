be2266cafd42e10945e5e8163472266584f1ab4d
sudiptapaul11 <99750216+sudiptapaul11@users.noreply.github.com>
2023-04-10 06:28:56 +0530

    Update sudipta.js

--- src/sudipta.js
+++ src/sudipta.js
@@ -3,16 +3,19 @@
 var mobile_button = document.querySelector(".mobile");
 var verticalMenu = document.querySelector(".verticalmenu");
 
+
+var x = document.querySelector(".verticalmenu");
+x.style.display = "none";
+
 mobile_button.onclick = () => {
     togglemobile();
-    console.log("abcd")
+    
 }
 
 
 
 
 function togglemobile() {
-    var x = document.querySelector(".verticalmenu");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
